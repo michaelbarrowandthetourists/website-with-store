fcd220395ce44c934dae081e5152841db5d65b49
Alessandro Improta <aimprota30@gmail.com>
2019-06-16 15:08:17 -0600

    Added netlify form honeypot

--- src/js/components/presentational/EmailCapture.jsx
+++ src/js/components/presentational/EmailCapture.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const EmailCapture = () => (
 	<form name="emails" data-netlify="true" netlify-honeypot="bot-field">
-		<input name="bot-field" styles="display: none;"/>
+		<input name="bot-field" style="display: none;"/>
 		<input className="email-capture" type="email" placeholder="ENTER EMAIL FOR UPDATES"></input>
 	</form>
 );
