6829dfbfb90a7c6b3bd3f93fd7e069a7462b44ef
Alessandro Improta <aimprota30@gmail.com>
2019-06-16 21:27:21 -0600

    adjusting form

--- src/js/components/presentational/EmailCapture.jsx
+++ src/js/components/presentational/EmailCapture.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 
 const EmailCapture = () => (
-	<form name="emails" method="POST" netlify-honeypot="bot-field" data-netlify="true">
+	<form name="email-form" method="POST" enctype="application/x-www-form-urlencoded" netlify-honeypot="bot-field" data-netlify="true">
 		<input className="hidden" name="bot-field" />
 		<input className="email-capture" type="email" placeholder="ENTER EMAIL FOR UPDATES"></input>
-		<div  ><input type="submit" tabindex="-1"/></div>
+		<div  ><input name="submit" type="submit" tabindex="-1"/></div>
 	</form>
 );
 
