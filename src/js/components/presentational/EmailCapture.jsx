ecb3d600b8800034295440c48478d4c04678da77
Alessandro Improta <aimprota30@gmail.com>
2019-06-17 11:48:14 -0600

    V-1.0

--- src/js/components/presentational/EmailCapture.jsx
+++ src/js/components/presentational/EmailCapture.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const EmailCapture = () => (
 	<form name="email-form" method="POST" enctype="application/x-www-form-urlencoded">
 		<input type="hidden" name="form-name" value="email-form" />
-		<input id="honeypot" name="bot-field" value="I'm stuck!!!" hidden></input>
+		<input name="bot-field" hidden></input>
 		<input className="email-capture" name="email-input" type="email" placeholder="ENTER EMAIL FOR UPDATES"></input>
 		<div className="hidden-submit"><input name="submit-button" type="submit" tabindex="-1"></input></div>
 	</form>
