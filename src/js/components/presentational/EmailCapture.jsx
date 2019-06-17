796db5fb4acecb083a4ac1aae880952faa669774
Alessandro Improta <aimprota30@gmail.com>
2019-06-17 11:33:45 -0600

    added static HTML form for netlify bots

--- src/js/components/presentational/EmailCapture.jsx
+++ src/js/components/presentational/EmailCapture.jsx
@@ -5,6 +5,7 @@ const EmailCapture = () => (
 		<input type="hidden" name="form-name" value="email-form" />
 		<input name="bot-field" hidden></input>
 		<input className="email-capture" name="email-input" type="email" placeholder="ENTER EMAIL FOR UPDATES"></input>
+		<div className="hidden-submit"><input name="submit-button" type="submit" tabindex="-1"></input></div>
 	</form>
 );
 
