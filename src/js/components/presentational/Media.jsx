4e7abe31be46147e2305a902cdc00cc120875863
alessandro <alessandro@gmetrix.com>
2021-01-30 18:19:31 -0700

    deleted comments

--- src/js/components/presentational/Media.jsx
+++ src/js/components/presentational/Media.jsx
@@ -32,45 +32,6 @@ export default function Media() {
                     <li className='grid-block'>
                         <img className="grid-image" alt="Band Photo" src={require('../../../images/7.jpg')}/>
                     </li>
-                    {/* <li className='grid-block'>
-                        <img className="grid-image" alt="Band Photo" src={require('../../../images/8.jpg')}/>
-                    </li>
-                    <li className='grid-block'>
-                        <img className="grid-image" alt="Band Photo" src={require('../../../images/9.jpg')}/>
-                    </li>
-                    <li className='grid-block'>
-                        <img className="grid-image" alt="Band Photo" src={require('../../../images/10.jpg')}/>
-                    </li>
-                    <li className='grid-block'>
-                        <img className="grid-image" alt="Band Photo" src={require('../../../images/11.jpg')}/>
-                    </li>
-                    <li className='grid-block'>
-                        <img className="grid-image" alt="Band Photo" src={require('../../../images/12.jpg')}/>
-                    </li>
-                    <li className='grid-block'>
-                        <img className="grid-image" alt="Band Photo" src={require('../../../images/13.jpg')}/>
-                    </li>
-                    <li className='grid-block'>
-                        <img className="grid-image" alt="Band Photo" src={require('../../../images/14.jpg')}/>
-                    </li>
-                    <li className='grid-block'>
-                        <img className="grid-image" alt="Band Photo" src={require('../../../images/15.jpg')}/>
-                    </li>
-                    <li className='grid-block'>
-                        <img className="grid-image" alt="Band Photo" src={require('../../../images/MB&T.jpg')}/>
-                    </li>
-                    <li className='grid-block'>
-                        <iframe className='u-full-width' height='150' src="https://www.youtube.com/embed/oCHFJ1tJpjg" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-                    </li>
-                    <li className='grid-block'>
-                        <img className="grid-image" alt="Band Photo" src={require('../../../images/16.jpg')}/>
-                    </li>
-                    <li className='grid-block'>
-                        <img className="grid-image" alt="Band Photo" src={require('../../../images/17.jpg')}/>
-                    </li>
-                    <li className='grid-block'>
-                        <img className="grid-image" alt="Band Photo" src={require('../../../images/MB&T-70.jpg')}/>
-                    </li> */}
                 </ul>
             </div>
         </div>
